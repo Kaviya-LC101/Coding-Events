a532b13df74e71d0d8ba47a5b5ae9bbc423abf70
kaviya <kaviya0925@gmail.com>
2021-10-13 11:26:10 -0500

    Starter Coding with Form

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,13 +17,16 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events= new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
+//        List<String> events = new ArrayList<>();
+//        events.add("Code With Pride");
+//        events.add("Strange Loop");
+//        events.add("Apple WWDC");
+//        ievents.add("SpringOne Platform");
+        //** above lines are commented as the create event redirect to displayall events
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -32,4 +37,15 @@ public class EventController {
     public String renderCreateEventForm(){
         return "events/create";
     }
+
+    //lives at /events/create
+    @PostMapping("create")
+   public String createEvent(@RequestParam String eventName) {
+        //the variable name 'eventName should match the /event/create template input text name
+        events.add(eventName);
+        return "redirect:";
+        // redirect to the path specified after the ':'.In this case since we have @RequestMapping("events") at the top this will redirect to local8080/events
+        //which will call above method displayAllEvents
+   }
+
 }
