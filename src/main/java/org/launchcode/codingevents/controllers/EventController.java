03f58b21041cf343142eeb6ed13d223f04ed1531
kaviya <kaviya0925@gmail.com>
2021-10-14 00:57:59 -0500

    Exercise 11.10 Thymeleaf views

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -17,35 +18,34 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events= new ArrayList<>();
-
+    //private static List<String> events = new ArrayList<>();
+    private static HashMap<String, String> events=new HashMap<>();
     @GetMapping
     public String displayAllEvents(Model model) {
 //        List<String> events = new ArrayList<>();
 //        events.add("Code With Pride");
 //        events.add("Strange Loop");
 //        events.add("Apple WWDC");
-//        ievents.add("SpringOne Platform");
-        //** above lines are commented as the create event redirect to displayall events
+//        events.add("SpringOne Platform");
+      //HashMap<String, String> events=new HashMap<>();
+        events.put("Menteaship","A fun meetup for connecting with mentors");
+        events.put("Code With Pride","A fun meetup sponsored by LaunchCode");
+        events.put("Javascripty", "An imaginary meetup for Javascript developers");
+        model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
         return "events/index";
     }
 
-
-    //lives at /events/create
     @GetMapping("create")
-    public String renderCreateEventForm(){
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
-    //lives at /events/create
     @PostMapping("create")
-   public String createEvent(@RequestParam String eventName) {
-        //the variable name 'eventName should match the /event/create template input text name
-        events.add(eventName);
+    public String processCreateEventForm(@RequestParam String eventName, String description) {
+        events.put(eventName,description);
         return "redirect:";
-        // redirect to the path specified after the ':'.In this case since we have @RequestMapping("events") at the top this will redirect to local8080/events
-        //which will call above method displayAllEvents
-   }
+    }
 
 }
