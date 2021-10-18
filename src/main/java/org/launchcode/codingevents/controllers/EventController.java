06c0b55966db18c01eb5a012fa27c3200dec5562
kaviya <kaviya0925@gmail.com>
2021-10-17 19:57:50 -0500

    Edit-events ExerciseChapter 12

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -4,10 +4,7 @@ import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Chris Bay
@@ -32,9 +29,8 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName,
-                                         @RequestParam String eventDescription) {
-        EventData.add(new Event(eventName, eventDescription));
+    public String processCreateEventForm(@ModelAttribute Event newEvent) {
+        EventData.add(newEvent);
         return "redirect:";
     }
     @GetMapping("delete")
@@ -53,5 +49,25 @@ public class EventController {
             }
         return "redirect:";
     }
+
+    @GetMapping("edit/{eventId}")
+    public String displayEditForm(Model model,@PathVariable int eventId){
+       Event eventToEdit=EventData.getById(eventId);
+        model.addAttribute("event",eventToEdit);
+        String title ="Edit Event " + eventToEdit.getName() + " (id=" + eventToEdit.getId() + ")";
+        model.addAttribute("title", title );
+
+        return "events/edit";
+    }
+
+
+    @PostMapping("edit")
+     public String processEditForm(int eventId, String name, String descrition){
+        Event eventToEdit=EventData.getById(eventId);
+        eventToEdit.setName(name);
+        eventToEdit.setDescription(descrition);
+        return "redirect:";
+    }
+
 
 }
